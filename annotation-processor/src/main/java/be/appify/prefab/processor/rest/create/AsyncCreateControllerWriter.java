78e74702250973a7604a2c18c674233b54ec6476
Stijn Van Bael <qtj487@fluvius.be>
2026-04-29 20:50:12 +0200

    feat(task-146): extract path parameters for @Create and @Update methods

--- annotation-processor/src/main/java/be/appify/prefab/processor/rest/create/AsyncCreateControllerWriter.java
+++ annotation-processor/src/main/java/be/appify/prefab/processor/rest/create/AsyncCreateControllerWriter.java
@@ -14,6 +14,7 @@ import com.palantir.javapoet.ParameterizedTypeName;
 import jakarta.validation.Valid;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import javax.lang.model.element.ExecutableElement;
 import org.springframework.http.ResponseEntity;

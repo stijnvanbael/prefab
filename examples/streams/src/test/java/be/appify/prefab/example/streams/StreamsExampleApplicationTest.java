1d1d411276b63d342f807cad564d1dfa4384f7d4
Stijn Van Bael <stijn.van.bael@appify.be>
2026-05-20 10:47:49 +0100

    feat[example/streams]: fix compiler error

--- examples/streams/src/test/java/be/appify/prefab/example/streams/StreamsExampleApplicationTest.java
+++ examples/streams/src/test/java/be/appify/prefab/example/streams/StreamsExampleApplicationTest.java
@@ -1,11 +1,10 @@
 package be.appify.prefab.example.streams;
 
-import be.appify.prefab.example.streams.infrastructure.kafka.StreamEventKafkaProducer;
+import be.appify.prefab.core.kafka.GenericKafkaProducer;
 import be.appify.prefab.test.EventConsumer;
 import be.appify.prefab.test.IntegrationTest;
 import be.appify.prefab.test.TestEventConsumer;
 import be.appify.prefab.test.asserts.EventAssertions;
-import java.util.concurrent.TimeUnit;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +12,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
+import java.util.concurrent.TimeUnit;
+
 @IntegrationTest
 class StreamsExampleApplicationTest {
     @Autowired
-    StreamEventKafkaProducer streamEventPublisher;
+    GenericKafkaProducer kafkaProducer;
 
     @TestEventConsumer(topic = "${topics.streams.words}")
     EventConsumer<WordEvent> wordsConsumer;
@@ -29,9 +30,9 @@ class StreamsExampleApplicationTest {
 
     @Test
     void branchAndMerge_shouldRouteWordsAndEmitMergedOutput() {
-        streamEventPublisher.publish(new StreamEvent("s-1", "hello,world,foo"));
-        streamEventPublisher.publish(new StreamEvent("s-2", "toolong,mini,tiny"));
-        streamEventPublisher.publish(new StreamEvent("s-3", "   "));
+        kafkaProducer.publish(new StreamEvent("s-1", "hello,world,foo"));
+        kafkaProducer.publish(new StreamEvent("s-2", "toolong,mini,tiny"));
+        kafkaProducer.publish(new StreamEvent("s-3", "   "));
 
         EventAssertions.assertThat(wordsConsumer)
                 .hasReceivedMessages(6)
